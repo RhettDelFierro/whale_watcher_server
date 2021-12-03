4149a5ec1740f59d9c238705ae5734ac738df9e4
Rhett Del Fierro <thinquetank@gmail.com>
2021-12-03 07:21:23 -0800

    test was not stopping so had to return the HttpServer after initiating it.

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ use whale_watcher_server::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    run().await
+    run()?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,7 +1,18 @@
-//! tests/health_check.rs
+#[actix_rt::test]
+async fn health_check_works() {
+    spawn_app().await.expect("Failed to spawn our app.");
+    let client = reqwest::Client::new();
+    let response = client
+        .get("http://127.0.0.1:8000/health_check")
+        .send()
+        .await
+        .expect("Failed to execute request.");
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
 
-use whale_watcher_server::main;
-#[test]
-fn dummy_test() {
-    main()
+// our integration test
+// basically going to run this test like it was a real user:
+async fn spawn_app() -> std::io::Result<()> {
+    whale_watcher_server::run().await
 }
