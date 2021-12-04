f454d66747f9b35bc63cb6f7c0bfaf241c1c626d
Rhett Del Fierro <rdelfierro@netfortris.com>
2021-12-04 05:00:43 -0800

    using db pool instead of a collection for the application state.

--- src/routes/holders.rs
+++ src/routes/holders.rs
@@ -1,15 +1,27 @@
 use actix_web::{web, HttpResponse};
+use sqlx::PgPool;
+use chrono::{DateTime, Utc};
+use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
 pub struct HolderData {
-    token_name: String,
-    contract_address: String,
+    token_name: String, // TODO: will still need to make a separate table
+    contract_address: String, // TODO: will still need to make a separate table
     holder_address: String,
-    place: u16,
-    amount: f64,
-    timestamp: u64,
+    place: u16, // TODO: will still need to make a separate table
+    amount: f64, // TODO: will still need to make a separate table
+    timestamp: i64, // TODO: will be from Utc::now().timestamp() from a time passed in by the front end.
 }
 
-pub async fn add_holder(_form: web::Form<HolderData>) -> HttpResponse {
+pub async fn add_holder(_form: web::Form<HolderData>, pool: web::Data<PgPool>) -> HttpResponse {
+    sqlx::query!(
+        r#"
+        INSERT INTO holders (id, holder_address) VALUES ($1, $2)
+        "#,
+        Uuid:: new_v4(),
+        _form.holder_address,
+    )
+        .execute(pool.get_ref())
+        .await;
     HttpResponse::Ok().finish()
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,16 +1,16 @@
 use crate::routes::{add_holder, health_check};
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer};
-use sqlx::PgConnection;
+use sqlx::PgPool;
 use std::net::TcpListener;
 
-pub fn run(listener: TcpListener, db_connection: PgConnection) -> Result<Server, std::io::Error> {
-    let connection = web::Data::new(db_connection);
+pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+    let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/holders", web::post().to(add_holder))
-            .app_data(connection.clone())
+            .app_data(db_pool.clone())
     })
     .listen(listener)?
     .run();
