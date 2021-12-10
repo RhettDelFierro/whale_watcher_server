6d694c2624369ccfa2645284f5888ba91445e9a6
Rhett Del Fierro <rdelfierro@netfortris.com>
2021-12-10 15:08:28 -0800

    adding new domain module.

--- src/routes/holders.rs
+++ src/routes/holders.rs
@@ -156,16 +156,6 @@ pub struct HoldersResponse {
     data: Vec<HolderData>,
 }
 
-// #[derive(serde::Deserialize, serde::Serialize)]
-// pub struct HolderResponse {
-//     network: String,
-//     token_name: String,
-//     contract_address: String,
-//     holder_address: String,
-//     place: i32,
-//     amount: BigDecimal,
-// }
-
 #[tracing::instrument(
 name = "Fetching holders.",
 skip(parameters, pool),
@@ -216,4 +206,12 @@ pub async fn get_holder(
             HttpResponse::InternalServerError().finish()
         }
     }
+}
+
+pub fn is_valid_name(s: &str) -> bool {
+    let is_empty_or_whitespace = s.trim().is_empty();
+    let is_too_long = s.graphemes(true).count() > 256;
+    let forbidden_characters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
+    let contains_forbidden_characters = s.chars().any(|g| forbidden_characters.contains(&g));
+    !(is_empty_or_whitespace || is_too_long || contains_forbidden_characters)
 }
