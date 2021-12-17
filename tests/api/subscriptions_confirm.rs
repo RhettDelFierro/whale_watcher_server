7930e103397a93f1e792868af72a6ea5d8d0c5e4
Rhett Del Fierro <rdelfierro@netfortris.com>
2021-12-17 09:22:02 -0800

    refactored tests

--- tests/api/subscriptions_confirm.rs
+++ tests/api/subscriptions_confirm.rs
@@ -14,7 +14,6 @@ async fn confirmations_without_token_are_rejected_with_a_400() {
 
 #[actix_rt::test]
 async fn the_link_returned_by_subscribe_returns_a_200_if_called() {
-    // Arrange
     let app = spawn_app().await;
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
     Mock::given(path("/email"))
@@ -22,26 +21,9 @@ async fn the_link_returned_by_subscribe_returns_a_200_if_called() {
         .respond_with(ResponseTemplate::new(200))
         .mount(&app.email_server)
         .await;
-
     app.post_subscriptions(body.into()).await;
     let email_request = &app.email_server.received_requests().await.unwrap()[0];
-    let body: serde_json::Value = serde_json::from_slice(&email_request.body).unwrap();
-
-    let get_link = |s: &str| {
-        let links: Vec<_> = linkify::LinkFinder::new()
-            .links(s)
-            .filter(|l| *l.kind() == linkify::LinkKind::Url)
-            .collect();
-        assert_eq!(links.len(), 1);
-        links[0].as_str().to_owned()
-    };
-
-    let raw_confirmation_link = &get_link(&body["HtmlBody"].as_str().unwrap());
-    let mut confirmation_link = Url::parse(raw_confirmation_link).unwrap();
-    assert_eq!(confirmation_link.host_str().unwrap(), "127.0.0.1");
-    confirmation_link.set_port(Some(app.port)).unwrap();
-    // We now have a confirmation link that points out out app at route subscriptions/confirm.
-    // Check that link with a get request:
-    let response = reqwest::get(confirmation_link).await.unwrap();
+    let confirmation_links = app.get_confirmation_links(&email_request);
+    let response = reqwest::get(confirmation_links.html).await.unwrap();
     assert_eq!(response.status().as_u16(), 200);
 }
